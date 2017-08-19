5103c4bfacca8d30916d57919790c5af00854b01
andrewarosario <andrew.arosario@gmail.com>
2017-08-19 11:33:45 -0300

    Regras de Validação dos ingredientes

--- src/pages/modal-ingredientes/modal-ingredientes.ts
+++ src/pages/modal-ingredientes/modal-ingredientes.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { Ingrediente } from "../../models/ingrediente/ingrediente.interface";
 import { FirebaseListObservable, AngularFireDatabase, FirebaseObjectObservable } from "angularfire2/database";
 import { Subscription } from "rxjs/Subscription";
+import { ToastController } from 'ionic-angular';
 
 @Component({
     selector: 'page-modal-ingredientes',
@@ -25,7 +26,8 @@ export class ModalIngredientesPage {
     constructor(public navCtrl: NavController, 
                 public navParams: NavParams,
                 private database: AngularFireDatabase,
-                public viewCtrl: ViewController) {
+                public viewCtrl: ViewController,
+                private toastCtrl: ToastController) {
 
         if (navParams.get('ingredienteId')) {
             this.possuiParametro = true;
@@ -45,10 +47,14 @@ export class ModalIngredientesPage {
         
     }
 
-    salvarIngrediente(ingrediente: Ingrediente) {
+    salvarIngrediente(ingrediente: Ingrediente) {                
+        if (!this.confereCampos(ingrediente)) {
+            return;
+        }
+
         /*
-          Cria um objeto anônimo e converte quantidade para number.
-          Dá um Push pro Firebase dentro da coleção 'ingrediente'
+        Cria um objeto anônimo e converte quantidade para number.
+        Dá um Push pro Firebase dentro da coleção 'ingrediente'
         */
 
         if (this.possuiParametro) {
@@ -63,7 +69,28 @@ export class ModalIngredientesPage {
             });
         }
 
-        this.fecharModal();
+        this.fecharModal();        
+    }
+
+    confereCampos(ingrediente: Ingrediente) {
+        try {
+            if (! this.ingrediente.nome) throw "Informe o nome do ingrediente!";
+            if (!this.ingrediente.quantidade || this.ingrediente.quantidade <= 0) throw "Informe uma quantidade!";
+
+            return true;
+        }
+        catch(err) {
+            let toast = this.toastCtrl.create( {
+                message: err, 
+                duration: 3000,
+                position: 'bottom',
+                showCloseButton: true,
+                closeButtonText: 'X'                
+            });
+            toast.present();
+
+            return false;          
+        }
     }
 
     fecharModal() {
@@ -79,3 +106,4 @@ export class ModalIngredientesPage {
     }    
 
 }
+
